252e2de67080a45dae0654b20992bd4629430e5f
sdrtba <sdrtba@yandex.ru>
2024-08-03 00:35:39 +0400

    rewrite zavod logic

--- Assets/Scripts/ZavodHandler.cs
+++ Assets/Scripts/ZavodHandler.cs
@@ -12,181 +12,174 @@ public class ZavodHandler : MonoBehaviour
     [Range(0, 1)][SerializeField] private float leverClipVolume;
     [SerializeField] private AudioClip pressClip;
     [Range(0, 1)][SerializeField] private float pressClipVolume;
-    [SerializeField] private AudioClip changeClip;
-    [Range(0, 1)][SerializeField] private float changeClipVolume;
+    [SerializeField] private AudioClip screenClip;
+    [Range(0, 1)][SerializeField] private float screenClipVolume;
 
     [Header("Coefficients")]
-    [Range(0, 100)][SerializeField] private int failImpact;
-    [Range(0, 100)][SerializeField] private int moneyImpact;
     [Range(0, 100)][SerializeField] private int hungerImpact;
-    [Range(0f, 100f)][SerializeField] private float pressSpeed;
-    [Range(0f, 100f)][SerializeField] private float prefabsSpeed;
-    [Range(0f, 100f)][SerializeField] private float itemsSpeed;
-    [Range(0f, 100f)][SerializeField] private float sliderSpeed;
-    [Range(0f, 1f)][SerializeField] private float successRangeValue;
+    [Range(0, 100)][SerializeField] private int moneyImpact;
+    [Range(0, 100)][SerializeField] private int failImpact;
+    [Range(0, 100)][SerializeField] private float screenItemsSpeed;
+    [Range(0, 100)][SerializeField] private float objectsSpeed;
+    [Range(0, 100)][SerializeField] private float pressSpeed;
+    [Range(0, 100)][SerializeField] private float sliderSpeed;
+    [Range(0, 100)][SerializeField] private float successRangeValue;
 
     [Header("System")]
     [SerializeField] private GameObject hungerCanvas;
     [SerializeField] private GameObject doneCanvas;
     [SerializeField] private Text doneText;
-    [SerializeField] private Text indexText;
-    [SerializeField] private RectTransform press;
-    [SerializeField] private GameObject[] objects;
-    [SerializeField] private GameObject[] items;
-    [SerializeField] private GameObject[] prefabs;
-    [SerializeField] private RectTransform prefabsParentTransform;
-    [SerializeField] private RectTransform waitingTransform;
-    [SerializeField] private GameObject itemsParent;
-    [SerializeField] private Transform[] imageItemsPoints;
+    [SerializeField] private Text scoreText;
+    [Space]
     [SerializeField] private Button nextBtn;
     [SerializeField] private Button prevBtn;
-    [SerializeField] private Slider slider;
+    [SerializeField] private Button leverBtn;
+    [Space]
+    [SerializeField] private GameObject[] items;
+    [SerializeField] private GameObject[] prefabs;
+    [SerializeField] private RectTransform[] objects;
+    [Space]
+    [SerializeField] private RectTransform[] screenItemsPosition;
+    [SerializeField] private RectTransform screenItemsParent;
+    [SerializeField] private RectTransform objectsParent;
+    [SerializeField] private RectTransform waitingTransform;
+    [SerializeField] private RectTransform press;
+    [Space]
     [SerializeField] private GameObject successRange;
-
+    [SerializeField] private Slider slider;
+    
+    private List<RectTransform> _objectsList = new List<RectTransform>();
     private List<int> _idList = new List<int>();
-    private bool _sliderMoveRight = true;
-    private bool _itemsMoveRight = false;
-    private bool _itemsMoveLeft = false;
-    private bool _canPressBtn = true;
+    private bool _canPressScreenBtn = true;
     private bool _canPress = false;
-    private float _offset = 0.1f;
-    private int _index = 0;
-    private int _id = 0;
-    private RectTransform _itemTransform;
+    private int _curId = 0;
     private RectTransform _sliderRectTransform;
+    private Sprite _defLeverSprite;
+    private Image _leverImage;
     private float _random;
     private float _defY;
+    private int _score;
 
 
     private void Start()
     {
+        AudioSource backAudioSource = SoundManager.instance.PlayAudioClip(backClip, transform, backClipVolume, false);
+        backAudioSource.loop = true;
+
         SkufHandler.instance.SetHUDVisibility(false);
         if (SkufHandler.instance.hunger <= 0) hungerCanvas.SetActive(true);
-
-        _itemTransform = prefabs[_index].GetComponent<RectTransform>();
-        _sliderRectTransform = slider.GetComponent<RectTransform>();
-        prevBtn.interactable = false;
-        _defY = press.position.y;
-
-        AudioSource windAudioSource = SoundManager.instance.PlayAudioClip(backClip, transform, backClipVolume, false);
-        windAudioSource.loop = true;
-
-        int rand;
-        for (int i = 0; i < prefabs.Length; i++)
+        else
         {
-            rand = Random.Range(0, items.Length);
-            _idList.Add(rand);
-            Instantiate(items[rand], prefabs[i].transform).GetComponent<Image>().raycastTarget = false;
-        }
+            SkufHandler.instance.SetText(doneText, hungerImpact.ToString(), moneyImpact.ToString());
 
-        ChangeRange();
-        StartCoroutine(MoveLine());
+            _sliderRectTransform = slider.GetComponent<RectTransform>();
+            _leverImage = leverBtn.GetComponent<Image>();
+            _defLeverSprite = _leverImage.sprite;
+
+            ChangeRange();
+            CheckButton();
+            CreateObjects();
+
+            StartCoroutine(MoveObjects());
+        }
     }
 
     private void Update()
     {
-        if (_itemsMoveRight)
-        {
-            itemsParent.transform.position = Vector3.Lerp(itemsParent.transform.position, imageItemsPoints[_id].position, itemsSpeed * Time.deltaTime);
-            if (itemsParent.transform.position.x + _offset >= imageItemsPoints[_id].position.x)
-            {
-                _itemsMoveRight = false;
-                _canPressBtn = true;
-            }
-        }
-        else if (_itemsMoveLeft)
-        {
-            itemsParent.transform.position = Vector3.Lerp(itemsParent.transform.position, imageItemsPoints[_id].position, itemsSpeed * Time.deltaTime);
-            if (itemsParent.transform.position.x - _offset <= imageItemsPoints[_id].position.x)
-            { 
-                _itemsMoveLeft = false;
-                _canPressBtn = true;
-            }
-        }
+        if ((Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) && prevBtn.interactable) Prev();
+        else if ((Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) && nextBtn.interactable) Next();
 
-        if (slider.value < 1 && _sliderMoveRight) slider.value += sliderSpeed * Time.deltaTime;
-        else _sliderMoveRight = false;
-        if (slider.value > 0 && !_sliderMoveRight) slider.value -= sliderSpeed * Time.deltaTime;
-        else _sliderMoveRight = true;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            leverBtn.onClick.Invoke();
+            _leverImage.sprite = leverBtn.spriteState.pressedSprite;
+        }
+        else if (Input.GetKeyUp(KeyCode.Space)) _leverImage.sprite = _defLeverSprite;
 
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            Prev();
-        }
-        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            Next();
-        }
+        float x = Time.time % sliderSpeed;
+        slider.value = (-Mathf.Abs(2 * x / sliderSpeed - 1) + 1);
     }
 
-    private IEnumerator MoveLine()
+    private IEnumerator StartPress(bool success)
     {
-        while (_itemTransform.position.x > waitingTransform.position.x)
+        AudioSource pressAudioSource = SoundManager.instance.PlayAudioClip(pressClip, transform, pressClipVolume, false);
+        while (press.anchoredPosition.y > waitingTransform.anchoredPosition.y)
         {
-            prefabsParentTransform.position -= new Vector3(prefabsSpeed * Time.deltaTime, 0f, 0f);
-            yield return null;
-        }
-        _canPress = true;
-    }
-
-    private IEnumerator Press(bool success)
-    {
-        AudioSource audioSource = SoundManager.instance.PlayAudioClip(pressClip, transform, pressClipVolume, false);
-        _canPress = false;
-        while (press.position.y > waitingTransform.position.y)
-        {
-            press.position -= new Vector3(0, pressSpeed * Time.deltaTime, 0);
+            press.Translate(Vector2.down * pressSpeed * Time.deltaTime);
             yield return null;
         }
 
-        Destroy(_itemTransform.GetChild(0).gameObject);
-        if (success) Instantiate(objects[_idList[0]], _itemTransform);
-        else Instantiate(objects[objects.Length-1], _itemTransform);
+        if (success) Instantiate(items[_curId], _objectsList[0].parent);
+        else Instantiate(items[items.Length-1], _objectsList[0].parent);
+        Destroy(_objectsList[0].gameObject);
+        _objectsList.RemoveAt(0);
         _idList.RemoveAt(0);
 
-        _index += 1;
-        indexText.text = _index + "/" + prefabs.Length;
-
-        audioSource.Stop();
-
-        while (press.position.y < _defY)
+        while (press.anchoredPosition.y < _defY)
         {
-            press.position += new Vector3(0, pressSpeed * Time.deltaTime, 0);
+            press.Translate(Vector2.up * pressSpeed * Time.deltaTime);
             yield return null;
         }
 
-        if (_index >= prefabs.Length)
+        pressAudioSource.Stop();
+
+        if (_objectsList.Count > 0)
         {
-            doneText.text = doneText.text.Replace("{0}", hungerImpact.ToString()).Replace("{1}", moneyImpact.ToString());
-            doneCanvas.SetActive(true);
-            SkufHandler.instance.ChangeHunger(-hungerImpact);
-            SkufHandler.instance.ChangeMoney(moneyImpact);
+            StartCoroutine(MoveObjects());
         }
         else
         {
-            _itemTransform = prefabs[_index].GetComponent<RectTransform>();
-            StartCoroutine(MoveLine());
+            doneCanvas.SetActive(true);
+            
+            SkufHandler.instance.ChangeHunger(-hungerImpact);
+            SkufHandler.instance.ChangeMoney(moneyImpact);
         }
     }
 
-    public void LeverPressed()
+    private IEnumerator MoveObjects()
     {
-        SoundManager.instance.PlayAudioClip(leverClip, transform, leverClipVolume);
-        if (_canPress)
-        {
-            if (slider.value >= _random && slider.value <= _random + successRangeValue && _id == _idList[0])
-            {
-                StartCoroutine(Press(true));
-            }
-            else
-            {
-                StartCoroutine(Press(false));
-                moneyImpact -= failImpact;
-            }
 
+        while (_objectsList[0].position.x > waitingTransform.position.x)
+        {
+            objectsParent.Translate(Vector2.left * objectsSpeed * Time.deltaTime);
+            yield return null;
         }
-        
-        ChangeRange();
+
+        _canPress = true;
+    }    
+
+    private IEnumerator MoveScreenItemsLeft()
+    {
+        while (screenItemsParent.anchoredPosition.x > screenItemsPosition[_curId].anchoredPosition.x)
+        {
+            screenItemsParent.Translate(Vector2.left * screenItemsSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        _canPressScreenBtn = true;
+    }
+
+    private IEnumerator MoveScreenItemsRight()
+    {
+        while (screenItemsParent.anchoredPosition.x < screenItemsPosition[_curId].anchoredPosition.x)
+        {
+            screenItemsParent.Translate(Vector2.right * screenItemsSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        _canPressScreenBtn = true;
+    }
+
+    private void CreateObjects()
+    {
+        int rand;
+        for (int i = 0; i < objects.Length; i++)
+        {
+            rand = Random.Range(0, prefabs.Length);
+            GameObject newObject = Instantiate(prefabs[rand], objects[i]);
+            _objectsList.Add(newObject.GetComponent<RectTransform>());
+            _idList.Add(rand);
+        }
     }
 
     private void ChangeRange()
@@ -198,42 +191,64 @@ public class ZavodHandler : MonoBehaviour
         successRangeRectTransform.anchoredPosition = new Vector3(_random * _sliderRectTransform.sizeDelta.x, 0, 0);
     }
 
-    public void Next()
-    {
-        if (_canPressBtn)
-        {
-            _canPressBtn = false;
-            _itemsMoveLeft = true;
-
-            _id += 1;
-            CheckButton();
-
-        }
-        
-    }
-
-    public void Prev()
-    {
-        if (_canPressBtn)
-        {
-            _canPressBtn = false;
-            _itemsMoveRight = true;
-
-            _id -= 1;
-            CheckButton();
-        }
-        
-    }
-
     private void CheckButton()
     {
-        SoundManager.instance.PlayAudioClip(changeClip, transform, changeClipVolume);
-        if (_id == 3) nextBtn.interactable = false;
-        else if (_id == 0) prevBtn.interactable = false;
+        SoundManager.instance.PlayAudioClip(screenClip, transform, screenClipVolume);
+
+        if (_curId == 3) nextBtn.interactable = false;
+        else if (_curId == 0) prevBtn.interactable = false;
         else
         {
             nextBtn.interactable = true;
             prevBtn.interactable = true;
+        }
+    }
+
+    public void LeverPressed()
+    {
+        SoundManager.instance.PlayAudioClip(leverClip, transform, leverClipVolume);
+
+        if (_canPress)
+        {
+            _score += 1;
+            scoreText.text = $"{_score}/{objects.Length}";
+            _canPress = false;
+
+            if (slider.value >= _random && slider.value <= _random + successRangeValue && _curId == _idList[0])
+            {
+                StartCoroutine(StartPress(true));
+            }
+            else
+            {
+                moneyImpact -= failImpact;
+                StartCoroutine(StartPress(false));
+            }
+        }
+
+        ChangeRange();
+    }
+
+    public void Next()
+    {
+        if (_canPressScreenBtn)
+        {
+            _curId += 1;
+            _canPressScreenBtn = false;
+
+            CheckButton();
+            StartCoroutine(MoveScreenItemsLeft());
+        }
+    }
+
+    public void Prev()
+    {
+        if (_canPressScreenBtn)
+        {
+            _curId -= 1;
+            _canPressScreenBtn = false;
+
+            CheckButton();
+            StartCoroutine(MoveScreenItemsRight());
         }
     }
 }
